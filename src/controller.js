068d8866305967e7cbcb9468e6cb903783198d86
shaurya <2019uee1692@mnit.ac.in>
2021-10-10 01:13:34 +0530

    added

--- src/controller.js
+++ src/controller.js
@@ -1,4 +1,8 @@
-const btn = document.getElementById("t");
-btn.onclick = () => {
-    console.log(document.getElementById("tt").value);
+function getval() {
+  let shape = document.getElementById("shape_list").value;
+  let gradient1 = document.getElementById("gradient1").value;
+  let gradient2 = document.getElementById("gradient2").value;
+
+  let image = document.getElementById("image").value;
+  let field = document.getElementById("field").value;
 }
