465af25d3f4eb7e16d6d2d40c96bb7d4da22741b
ayushk7 <ayushkashyap.rock2@gmail.com>
2021-10-10 08:49:05 +0530

    shape and bg done

--- src/controller.js
+++ src/controller.js
@@ -1,8 +1,55 @@
-function getval() {
-  let shape = document.getElementById("shape_list").value;
-  let gradient1 = document.getElementById("gradient1").value;
-  let gradient2 = document.getElementById("gradient2").value;
-
-  let image = document.getElementById("image").value;
-  let field = document.getElementById("field").value;
+import { Badge } from "./model.js";
+function getValues() {
+    let badgeData = {background: {}};
+    badgeData.shape_name = document.getElementById("shape_list").value;
+    badgeData.background.color = document.getElementById("color_check").value;
+    badgeData.background.color1 = document.getElementById("gradient1").value;
+    badgeData.background.color2 = document.getElementById("gradient2").value;
+    if(active_bg_option === 'bg-image'){
+        if(document.getElementById("image_ip").files.length !== 0){
+            badgeData.background.image_url = URL.createObjectURL(document.getElementById("image_ip").files[0]);
+        }
+        else{
+            alert("upload the file first");
+        }
+    }
+    badgeData.field = document.getElementById("field").value;
+    badgeData.background.type = active_bg_option;
+    // badgeData.organization_name = document.getElementById("")                                                            
+    return badgeData;
 }
+
+const bg_selection = document.getElementById("bg-selection-type");
+const bg_options = {
+    'bg-color': document.getElementById("bg-color"),
+    'bg-gradient': document.getElementById("bg-gradient"),
+    'bg-image': document.getElementById("bg-image"),
+};
+
+function makeBgOptionsHidden(){
+    for(let item in bg_options){
+        bg_options[item].classList.toggle("hidden", true);
+    }
+}
+// console.log(bg_selection, bg_options);
+let active_bg_option = 'bg-color';
+bg_selection.addEventListener("input", () => {
+    makeBgOptionsHidden();
+    const v = bg_selection.value;
+    console.log(v);
+    bg_options[v].classList.toggle("hidden", false);
+    active_bg_option = v;
+});
+
+const generate_btn = document.getElementById("generate-badge-btn");
+const bg_panel = document.getElementById("badge-panel");
+generate_btn.addEventListener("click", () => {
+    const badge_data = getValues();
+    const badge_gen = new Badge(badge_data);
+    bg_panel.innerHTML = '';
+    bg_panel.append(badge_gen.outer_div);
+});
+
+
+
+
--- src/model.js
+++ src/model.js
@@ -1,41 +1,49 @@
-class Badge {
-    constructor(shape_name, background, field, organization_name, tagline, logo_image) {
+export class Badge {
+    constructor({shape_name, background, field, organization_name, tagline, logo_image}) {
         this.outer_div = document.createElement("div");
         this.setShape(shape_name);
-        this.setBackgorund(background);
-
+        this.setBackground(background);
     }
     setShape(shape_name) {
         shape_name = shape_name.toLowerCase();
-        this.outer_div.classList = [];
+        this.outer_div.classList.toggle("badge", true);
         switch (shape_name) {
-            case "square": this.outer_div.classList.add("badge");
+            case "square": this.outer_div.classList.toggle("badge", true);
                 break;
-            case "rhombus": this.outer_div.classList.add("badge-rhombus");
+            case "rhombus": this.outer_div.classList.toggle("badge-rhombus", true);
                 break;
-            case "circle": this.outer_div.classList.add("badge-circle");
+            case "circle": this.outer_div.classList.toggle("badge-circle", true);
                 break;
-            case "oval": this.outer_div.classList.add("badge-oval");
+            case "oval": this.outer_div.classList.toggle("badge-oval", true);
                 break;
-            case "triagle": this.outer_div.classList.add("badge-triangle");
+            case "triangle": this.outer_div.classList.toggle("badge-triangle", true);
                 break;
-            case "octagon": this.outer_div.classList.add("badge-octagon");
+            case "octagon": this.outer_div.classList.toggle("badge-octagon", true);
                 break;
-            case "star": this.outer_div.classList.add("badge-star");
+            case "star": this.outer_div.classList.toggle("badge-star", true);
                 break;
         }
     }
-    setBackgorund(background){
-        if(background.type === "color"){
+    setBackground(background){
+        if(background.type === "bg-color"){
             this.outer_div.style.backgroundColor = background.color;
         }
-        else if(background.type === "gradient"){
-            this.outer_div.style.backgroundColor = `linear-gradient(to bottom right, ${background.color1}, ${background.color2});`;
+        else if(background.type === "bg-gradient"){
+            this.outer_div.style.background = `linear-gradient(to bottom right, ${background.color1}, ${background.color2})`;
+            // getComputedStyle(this.outer_div);
         }
         else{
-            this.outer_div.style.backgroundImage = background.image_name;
+            this.outer_div.style.backgroundImage = `url(${background.image_url})`;
+            this.outer_div.style.backgroundPosition = `center`;
+            this.outer_div.style.backgroundSize = `cover`;
+            // console.log(this.outer_div.style.backgroundImage);
         }
     }
+    getElement(){
+        return this.outer_div;
+    }
+
+    
 
 }
 //background: #053423;
